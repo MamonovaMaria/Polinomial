0dd6e871b52e6590e831eb72daebf35ee45e31a9
Mariya <mmamonova95@gmail.com>
2017-03-21 11:19:38 +0300

    no -

---
+++ Polynomials/Polynomials/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Polynomials
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.firstPolinomial = new System.Windows.Forms.TextBox();
+			this.secondPolinomial = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.buttonForSecomdtPolimom = new System.Windows.Forms.Button();
+			this.buttonForFirstPolimom = new System.Windows.Forms.Button();
+			this.buttonForSummation = new System.Windows.Forms.Button();
+			this.buttonForSubtraction = new System.Windows.Forms.Button();
+			this.textBoxForResult = new System.Windows.Forms.TextBox();
+			this.buttonForSave = new System.Windows.Forms.Button();
+			this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+			this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
+			this.buttonForStart = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// firstPolinomial
+			// 
+			this.firstPolinomial.Location = new System.Drawing.Point(12, 52);
+			this.firstPolinomial.Multiline = true;
+			this.firstPolinomial.Name = "firstPolinomial";
+			this.firstPolinomial.Size = new System.Drawing.Size(329, 94);
+			this.firstPolinomial.TabIndex = 2;
+			this.firstPolinomial.TextChanged += new System.EventHandler(this.firstPolinomial_TextChanged);
+			// 
+			// secondPolinomial
+			// 
+			this.secondPolinomial.Location = new System.Drawing.Point(375, 52);
+			this.secondPolinomial.Multiline = true;
+			this.secondPolinomial.Name = "secondPolinomial";
+			this.secondPolinomial.Size = new System.Drawing.Size(337, 94);
+			this.secondPolinomial.TabIndex = 3;
+			this.secondPolinomial.TextChanged += new System.EventHandler(this.secondPolinomial_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(169, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(385, 24);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Введите коэффициенты многочленов";
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialogForLoad";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+			// 
+			// buttonForSecomdtPolimom
+			// 
+			this.buttonForSecomdtPolimom.Location = new System.Drawing.Point(375, 152);
+			this.buttonForSecomdtPolimom.Name = "buttonForSecomdtPolimom";
+			this.buttonForSecomdtPolimom.Size = new System.Drawing.Size(75, 23);
+			this.buttonForSecomdtPolimom.TabIndex = 5;
+			this.buttonForSecomdtPolimom.Text = "Загрузить";
+			this.buttonForSecomdtPolimom.UseVisualStyleBackColor = true;
+			this.buttonForSecomdtPolimom.Click += new System.EventHandler(this.buttonForSecomdtPolimom_Click);
+			// 
+			// buttonForFirstPolimom
+			// 
+			this.buttonForFirstPolimom.Location = new System.Drawing.Point(12, 152);
+			this.buttonForFirstPolimom.Name = "buttonForFirstPolimom";
+			this.buttonForFirstPolimom.Size = new System.Drawing.Size(75, 23);
+			this.buttonForFirstPolimom.TabIndex = 6;
+			this.buttonForFirstPolimom.Text = "Загрузить";
+			this.buttonForFirstPolimom.UseVisualStyleBackColor = true;
+			this.buttonForFirstPolimom.Click += new System.EventHandler(this.buttonForFirstPolimom_Click);
+			// 
+			// buttonForSummation
+			// 
+			this.buttonForSummation.Enabled = false;
+			this.buttonForSummation.Location = new System.Drawing.Point(114, 242);
+			this.buttonForSummation.Name = "buttonForSummation";
+			this.buttonForSummation.Size = new System.Drawing.Size(218, 22);
+			this.buttonForSummation.TabIndex = 7;
+			this.buttonForSummation.Text = "Сложить";
+			this.buttonForSummation.UseVisualStyleBackColor = true;
+			this.buttonForSummation.Click += new System.EventHandler(this.buttonForSummation_Click);
+			// 
+			// buttonForSubtraction
+			// 
+			this.buttonForSubtraction.Enabled = false;
+			this.buttonForSubtraction.Location = new System.Drawing.Point(387, 242);
+			this.buttonForSubtraction.Name = "buttonForSubtraction";
+			this.buttonForSubtraction.Size = new System.Drawing.Size(210, 22);
+			this.buttonForSubtraction.TabIndex = 8;
+			this.buttonForSubtraction.Text = "Вычесть";
+			this.buttonForSubtraction.UseVisualStyleBackColor = true;
+			this.buttonForSubtraction.Click += new System.EventHandler(this.buttonForSubtraction_Click);
+			// 
+			// textBoxForResult
+			// 
+			this.textBoxForResult.Enabled = false;
+			this.textBoxForResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBoxForResult.Location = new System.Drawing.Point(54, 287);
+			this.textBoxForResult.Multiline = true;
+			this.textBoxForResult.Name = "textBoxForResult";
+			this.textBoxForResult.Size = new System.Drawing.Size(621, 136);
+			this.textBoxForResult.TabIndex = 9;
+			// 
+			// buttonForSave
+			// 
+			this.buttonForSave.Enabled = false;
+			this.buttonForSave.Location = new System.Drawing.Point(323, 429);
+			this.buttonForSave.Name = "buttonForSave";
+			this.buttonForSave.Size = new System.Drawing.Size(75, 23);
+			this.buttonForSave.TabIndex = 10;
+			this.buttonForSave.Text = "Сохранить";
+			this.buttonForSave.UseVisualStyleBackColor = true;
+			this.buttonForSave.Click += new System.EventHandler(this.buttonForSave_Click);
+			// 
+			// openFileDialog2
+			// 
+			this.openFileDialog2.FileName = "openFileDialogForLoad";
+			this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+			// 
+			// openFileDialog3
+			// 
+			this.openFileDialog3.FileName = "openFileDialogForLoad";
+			this.openFileDialog3.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog3_FileOk);
+			// 
+			// buttonForStart
+			// 
+			this.buttonForStart.Location = new System.Drawing.Point(12, 194);
+			this.buttonForStart.Name = "buttonForStart";
+			this.buttonForStart.Size = new System.Drawing.Size(708, 22);
+			this.buttonForStart.TabIndex = 11;
+			this.buttonForStart.Text = "Начать";
+			this.buttonForStart.UseVisualStyleBackColor = true;
+			this.buttonForStart.Click += new System.EventHandler(this.buttonForStart_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(724, 464);
+			this.Controls.Add(this.buttonForStart);
+			this.Controls.Add(this.buttonForSave);
+			this.Controls.Add(this.textBoxForResult);
+			this.Controls.Add(this.buttonForSubtraction);
+			this.Controls.Add(this.buttonForSummation);
+			this.Controls.Add(this.buttonForFirstPolimom);
+			this.Controls.Add(this.buttonForSecomdtPolimom);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.secondPolinomial);
+			this.Controls.Add(this.firstPolinomial);
+			this.Name = "Form1";
+			this.Text = "Polynomials";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox firstPolinomial;
+        private System.Windows.Forms.TextBox secondPolinomial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonForSecomdtPolimom;
+        private System.Windows.Forms.Button buttonForFirstPolimom;
+        private System.Windows.Forms.Button buttonForSummation;
+        private System.Windows.Forms.Button buttonForSubtraction;
+        private System.Windows.Forms.TextBox textBoxForResult;
+        private System.Windows.Forms.Button buttonForSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+		private System.Windows.Forms.Button buttonForStart;
+	}
+}
+
---
+++ Polynomials/Polynomials/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Polynomials
+{
+    public partial class Form1 : Form
+    {
+        List<List<int>> fPol;
+        List<List<int>> sPol;
+		List<List<int>> r;
+		List<int> N;
+		
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonForFirstPolimom_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void buttonForSecomdtPolimom_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+
+		private void firstPolinomial_TextChanged(object sender, EventArgs e)
+		{
+		}
+
+		private void secondPolinomial_TextChanged(object sender, EventArgs e)
+		{
+		}
+
+
+		private void buttonForStart_Click(object sender, EventArgs e)
+		{
+			buttonForSubtraction.Enabled = true;
+			buttonForSummation.Enabled = true;
+			buttonForSave.Enabled = true;
+		}
+
+		private void buttonForSummation_Click(object sender, EventArgs e)
+		{
+			fPol = new List<List<int>>();
+			sPol = new List<List<int>>();
+			N = new List<int>();
+			r = new List<List<int>>();
+			List<int> result = new List<int>();
+
+			createPol(firstPolinomial.Text, fPol);
+			createPol(secondPolinomial.Text, sPol);
+			balancingPolinomial();
+			textBoxForResult.Text = summ(result);
+			createPol(textBoxForResult.Text, r);
+		}
+
+		private void buttonForSubtraction_Click(object sender, EventArgs e)
+		{
+			fPol = new List<List<int>>();
+			sPol = new List<List<int>>();
+			N = new List<int>();
+			r = new List<List<int>>();
+			List<int> result = new List<int>();
+
+			createPol(firstPolinomial.Text, fPol);
+			createPol(secondPolinomial.Text, sPol);
+			balancingPolinomial();
+			textBoxForResult.Text = sub(result);
+			createPol(textBoxForResult.Text, r);
+		}
+
+		private void buttonForSave_Click(object sender, EventArgs e)
+		{
+			openFileDialog3.ShowDialog();
+		}
+
+
+		private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+			StreamReader openFileForFirstPolonomial = new StreamReader(openFileDialog1.FileName, Encoding.Default);
+			firstPolinomial.AppendText(openFileForFirstPolonomial.ReadToEnd());
+			openFileForFirstPolonomial.Close();
+		}
+
+		private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+		{
+			StreamReader openFileForSecondPolonomial = new StreamReader(openFileDialog2.FileName, Encoding.Default);
+			secondPolinomial.AppendText(openFileForSecondPolonomial.ReadToEnd());
+			openFileForSecondPolonomial.Close();
+		}
+
+		private void openFileDialog3_FileOk(object sender, CancelEventArgs e)
+		{
+			StreamWriter saveFale = new StreamWriter(openFileDialog3.FileName);
+			saveFale.WriteLine(textBoxForResult.Text);
+			saveFale.Close();
+		}
+
+
+		private void createPol(string text, List<List<int>> list)
+		{
+			string[] coefficients = text.Split(' ');
+
+			for (int j = 0; j < coefficients.Length; j++)
+			{
+				List<int> coefficient = new List<int>();
+				for (int i = 0; i < coefficients[j].Length; i++)
+					coefficient.Add(int.Parse(coefficients[j][i].ToString()));
+
+				list.Add(coefficient);
+			}
+		}
+
+		private void balancingPolinomial()
+		{
+			int max = 0;
+			if (fPol.Count < sPol.Count) max = sPol.Count;
+			else max = fPol.Count;
+
+			N.Add(0);
+
+			if (fPol.Count < max)
+				do
+				{
+					fPol.Insert(0, N);
+				} while (fPol.Count < max);
+			else if (sPol.Count < max)
+				do
+				{
+					sPol.Insert(0, N);
+				} while (sPol.Count < max);
+		}
+
+		private void balancingCoefficient(int i)
+		{
+			int max = 0;
+			if (fPol[i].Count < sPol[i].Count) max = sPol[i].Count;
+			else max = fPol[i].Count;
+
+			if (fPol[i].Count < max)
+				do
+				{
+					fPol[i].Insert(0, 0);
+				} while (fPol[i].Count < max);
+			else if (sPol[i].Count < max)
+				do
+				{
+					sPol[i].Insert(0, 0);
+				} while (sPol[i].Count < max);
+		}
+
+		public void print(List<List<int>> list)
+		{
+			textBoxForResult.Text = null;
+			for (int i = 0; i < list.Count; i++)
+			{
+				for (int j = 0; j < list[i].Count; j++)
+					textBoxForResult.AppendText(list[i][j].ToString());
+				textBoxForResult.AppendText(", ");
+			}
+		}
+
+		public string summ(List<int> result)
+		{
+			string res = "";
+			bool flag = false;
+			for (int i = 0; i < fPol.Count; i++)
+			{
+				result = new List<int>();
+				balancingCoefficient(i);
+				for (int j = fPol[i].Count - 1; j >= 0; j--)
+				{
+					int sum = fPol[i][j] + sPol[i][j];
+					if (sum >= 10)
+					{
+						result.Insert(0, sum - 10);
+
+						for (int k = 1;  ; k++)
+						{
+							if (j - k < 0)
+							{
+								flag = true;
+								break;
+							}
+
+							if(fPol[i][j - k] >= 9)
+								fPol[i][j - k] = 0;
+							else
+							{
+								fPol[i][j - k] += 1;
+								break;
+							}
+						}
+					}
+					else result.Insert(0, sum);
+				}
+				if (flag)
+					res += "1";
+				foreach (var el in result)
+					res += el;
+				res += " ";
+			}
+			return res;
+		}
+
+		public string sub(List<int> result)
+		{
+			List<int> a = new List<int>();
+			List<int> b = new List<int>();
+			string res = "";
+			bool flag1 = false;
+			bool flag2 = false;
+			for (int i = 0; i < fPol.Count; i++)
+			{
+				result = new List<int>();
+				balancingCoefficient(i);
+				for(int l = i; l < fPol.Count; l++)
+				{
+					if (fPol[i][0] > sPol[i][0])
+					{
+						a = fPol[i];
+						b = sPol[i];
+						break;
+					}
+					else if (fPol[i][0] < sPol[i][0])
+					{
+						a = sPol[i];
+						b = fPol[i];
+						flag2 = true;
+						break;
+					}
+				}
+				/////////////////////////////////////////
+				for (int j = fPol[i].Count - 1; j >= 0; j--)
+				{
+					int sub = a[j] - b[j];
+					if (sub >= 10)
+					{
+						result.Insert(0, sub - 10);
+
+						for (int k = 1; ; k++)
+						{
+							if (j - k < 0)
+							{
+								flag1 = true;
+								break;
+							}
+
+							if (fPol[i][j - k] >= 9)
+								fPol[i][j - k] = 0;
+							else
+							{
+								fPol[i][j - k] += 1;
+								break;
+							}
+						}
+					}
+					else result.Insert(0, sub);
+				}
+				if (flag2)
+					res += "-";
+				if (flag1)
+					res += "1";
+				foreach (var el in result)
+					res += el;
+				res += " ";
+				flag1 = false;
+				flag2 = false;
+			}
+			return res;
+		}
+	}
+}
